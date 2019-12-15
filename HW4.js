10b572b765e534a7a75e995cdfd065abf8209de8
valeryho <38451179+valeryho@users.noreply.github.com>
2019-12-15 16:50:51 +0200

    HW4

---
+++ HW4.js
@@ -0,0 +1,24 @@
+var v = prompt("Введіть ім'я: ", "");
+name1.innerHTML = name1.innerHTML + "<font color='black'>"+v+"</font>";
+v = prompt("Введіть свій вік: ",18);
+age1.innerHTML = age1.innerHTML + "<font color='black'>"+v+"</font>";
+v = prompt("Введіть номер телефона: ", "");
+telephone1.innerHTML = telephone1.innerHTML + "<font color='black'>"+v+"</font>";
+v = prompt("Введіть email: ", "");
+email1.innerHTML = email1.innerHTML + "<font color='black'>"+v+"</font>";
+
+name1.style.backgroundColor = "LightBlue";
+name1.style.fontFamily = "sans-serif";
+name1.style.marginTop = "5px";
+name1.style.marginBottom = "5px";
+
+age1.style.backgroundColor = "Aquamarine";
+age1.style.fontFamily = "sans-serif";
+
+telephone1.style.backgroundColor = "LightBlue";
+telephone1.style.fontFamily = "sans-serif";
+telephone1.style.marginTop = "5px";
+telephone1.style.marginBottom = "5px";
+
+email1.style.backgroundColor = "Aquamarine";
+email1.style.fontFamily = "sans-serif";
